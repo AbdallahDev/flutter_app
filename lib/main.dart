d0780b09c17d6ee8e966d15cac1127303237d22c
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-11 15:31:36 +0300

    Revert "Reset to 16"
    This reverts commit e20e63e9

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,16 @@ class MyApp extends StatelessWidget {
         //Test 1
         //Test 2
         //Test 3
+        //Test 4
+        //Test 5
+        //Test 6
+        //Test 7
+        //Test 8
+        //Test 9
+        //Test 10
+        //Test 11
+        //Test 15
+        //Test 16
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
