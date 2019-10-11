c12c65503448e4fcf33f96e98cecf508e7c05c25
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-11 15:26:21 +0300

    Revert "Revert "Test 12""
    This reverts commit 9160da8f

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,19 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         //Test 1
+        //Test 2
+        //Test 3
+        //Test 4
+        //Test 5
+        //Test 6
+        //Test 7
+        //Test 8
+        //Test 9
+        //Test 10
+        //Test 11
+        //Test 12
+        //Test 13
+        //Test 14
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
